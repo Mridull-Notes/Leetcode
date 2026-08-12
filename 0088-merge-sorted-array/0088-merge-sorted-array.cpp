f19e1ca618e7bd46fca663a2ee3f7b18cad3ec51
Mridull-Notes <mridul19072006@gmail.com>
2026-08-13 00:01:56 +0530

    Time: 0 ms (100%), Space: 12.6 MB (12.64%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,28 +1,211 @@
 class Solution {
 public:
     void merge(vector<int>& A, int m, vector<int>& B, int n) {
-       int idx=(m+n)-1;int i=m-1;int j=n-1;
-      while( i>=0 &&  j>=0){
-            if(B[j]>A[i]){
-                A[idx]=B[j];
-                j--;
-                idx--;
-            }
+       int i=m-1;
+       int j=n-1;
+       int k=m+n-1;
 
-            else{
-                A[idx]=A[i];
-                i--;
-                idx--;
-            }
+       while(i>=0 && j>=0){
+        if(A[i]<B[j]){
+        A[k]=B[j];
+        j--;
         }
 
-        while(j>=0){
-            A[idx]=B[j];
-            j--;
-            idx--;
+        else{ A[k]=A[i];
+        i--;}
 
-        }
-       
+        k--;}
         
-    }
-};
+
+        while(i<j && j>=0){
+            A[k]=B[j];
+            j--;
+            k--;
+        }
+
+        while(j<i && i>0){
+            A[k]=A[i];
+            i--;
+            k--;
+        }}
+
+}
+;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
