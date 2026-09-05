d9329dc6200839a67838826b495ba3dffd69143d
Mridull-Notes <mridul19072006@gmail.com>
2026-09-05 23:41:24 +0530

    Time: 3 ms (28.73%), Space: 28.3 MB (44.34%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count=0;
+        int ans;
+        for(int num:nums){
+             if(count==0){
+                ans=num;
+
+            }
+           
+            if(num==ans){
+                count++;
+            }
+
+            else{
+                count--;
+            }
+
+            
+
+        }
+        return ans;}
+};
